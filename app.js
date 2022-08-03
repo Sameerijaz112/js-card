ad4178e7af137370eae894b23e6951af5e2f9a39
Sameerijaz112 <sameerejaz970@gmail.com>
2022-08-03 02:22:00 -0700

    add

--- app.js
+++ app.js
@@ -1,23 +1,79 @@
-let creatCArd = () => {
+let CreateCard = () => {
+  try {
+    let CardArray = [];
+    let LocalStorageData = JSON.parse(localStorage.getItem('card-data'));//Main check karon k wo empty hai k nahi
+    let Name = document.querySelector('.Name').value;
+    let ImageUrl = document.querySelector('.ImageUrl').value;
+    let CardContainer = document.querySelector('.CardContainer');
+    let CardObject = new Object();
+    CardObject.Name = Name;
+    CardObject.ImageUrl = ImageUrl;
+    CardObject.Id = Math.floor(Math.random() * 1000);
+    CardObject.Points = 0;
 
-let Name =document.querySelector('.Name').value;
-let imageUrl =document.querySelector('.imageUrl').value;
-let cardobject = new Object();
+    //Agar Reload Hoo Jae
+    if (CardObject.Name === '' || CardObject.ImageUrl === '') {
+      let TemporaryLocalStorage = JSON.parse(localStorage.getItem('card-data'));
+      TemporaryLocalStorage.forEach(element => {
+        CardContainer.innerHTML += `
+        <div class="col mx-3 my-3">
+        <div class="card" style="width: 18rem;">
+        <img src="${element.ImageUrl}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p>Name: ${element.Name}</p>
+          <p>Points: ${element.Points}</p>
+        </div>
+            </div>
+        </div>
+    `
+      })
+      return
+    }
 
-cardobject.Name=Name;
-cardobject.imageUrl=imageUrl;
-cardobject.id = Math.floor(Math.random() * 1000);
-cardobject.point = 0;
-let conatinercard = document.querySelector('.card');
-conatinercard.innerHTML =
+    if (LocalStorageData === null || LocalStorageData.length === 0) {
+      // CardArray = [];
+      CardArray.push(CardObject);
+      localStorage.setItem('card-data', JSON.stringify(CardArray));
+      CardContainer.innerHTML = `
+    <div class="col mx-3 my-3">
+          <div class="card" style="width: 18rem;">
+          <img src="${CardArray.ImageUrl}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p>Name: ${CardArray.Name}</p>
+            <p>Points: ${CardArray.Points}</p>
+          </div>
+              </div>
+          </div>
+    `
+      return
+    }
 
-`<div class="card" style="width: 18rem;">
- <img src="${cardobject.imageUrl}" class="card-img-top" alt="...">
-<div class="card-body">
-
-  <h5 class="card-title">Card title</h5>
- <p>Name = ${cardobject.Name}</p>
- <p>point= ${cardobject.point}</p>
-</div>`
-
+    if (LocalStorageData.length !== 0) {
+      // CardArray = [];
+      LocalStorageData.forEach(element => {
+        CardArray.push(element);
+      });
+      CardArray.push(CardObject);
+      console.log(CardArray);
+      localStorage.setItem('card-data', JSON.stringify(CardArray));
+      let TemporaryLocalStorage = JSON.parse(localStorage.getItem('card-data'));
+      TemporaryLocalStorage.forEach(element => {
+        CardContainer.innerHTML += `
+          <div class="col mx-3 my-3">
+          <div class="card" style="width: 18rem;">
+          <img src="${element.ImageUrl}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p>Name: ${element.Name}</p>
+            <p>Points: ${element.Points}</p>
+          </div>
+              </div>
+          </div>
+    `
+      })
+      return
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  alert()
 }
